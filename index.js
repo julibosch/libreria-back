25df6a3412b5b92091b33e147477cdba5f47be86
JuliBosch <julibosch99@gmail.com>
2023-07-25 18:46:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor funcando en el puerto: http://localhost:${PORT}`);
+})
+
